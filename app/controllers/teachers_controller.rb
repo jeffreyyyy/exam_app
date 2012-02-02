75b10eadcc9ae7ec47dafb4cbbc945a07c1ac572
Jeffrey Jurgajtis <jeffreyj789@hotmail.com>
2012-02-01 21:31:02 -0600

    updated routes

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -4,6 +4,7 @@ class TeachersController < ApplicationController
   end
   
   def new
+    @teacher = Teacher.new
   end
   
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 ExamApp::Application.routes.draw do
   
-  get "teachers/new"
-
   resources :teachers
   
-  # match '/signup', :to => 'teachers#new'
+  match '/signup', :to => 'teachers#new'
   # root :to => 'welcome#index'
 
 end
